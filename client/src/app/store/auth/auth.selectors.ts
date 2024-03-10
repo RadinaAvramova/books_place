1da4658f0861fccebe90f0a49e067b0b90c71970
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-03-10 14:26:04 +0200

    Delete client directory

--- client/src/app/store/auth/auth.selectors.ts
+++
@@ -1,6 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { AuthState } from './user.model';
-
-const getAuthState = createFeatureSelector<AuthState>('auth');
-
-export const getUserData = createSelector(getAuthState, (state) => state.user);
