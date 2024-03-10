c42656f688d6578cdced265ea55d94a05b6c97a7
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-03-10 13:18:59 +0200

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,38 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'books-place-c5f24',
+    appId: '1:810236930063:web:cd05f8d99337b3ae4a1b7a',
+    databaseURL: 'https://books-place-c5f24-default-rtdb.firebaseio.com',
+    storageBucket: 'books-place-c5f24.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAvVbCm0m8G3Wlb-LZxXsAS4EVO5I7234Q',
+    authDomain: 'books-place-c5f24.firebaseapp.com',
+    messagingSenderId: '810236930063',
+    measurementId: 'G-6R18PZJD22',
+  },
+  // firebase: {
+  //   projectId: 'books-place-c5f24',
+  //   appId: '1:810236930063:web:cd05f8d99337b3ae4a1b7a',
+  //   databaseURL: 'https://books-place-c5f24-default-rtdb.firebaseio.com',
+  //   storageBucket: 'books-place-c5f24.appspot.com',
+  //   locationId: 'europe-west',
+  //   apiKey: 'AIzaSyAvVbCm0m8G3Wlb-LZxXsAS4EVO5I7234Q',
+  //   authDomain: 'books-place-c5f24.firebaseapp.com',
+  //   messagingSenderId: '810236930063',
+  //   measurementId: 'G-6R18PZJD22',
+  // },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
